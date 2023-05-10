449fdcfafcc3090115542a88352b221c4e156196
Chris Fang <cshfang@gmail.com>
2023-05-09 20:52:54 -0700

    fix(clients): import URL polyfill for RN

---
+++ packages/core/src/AwsClients/Pinpoint/index.native.ts
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+import 'react-native-url-polyfill/auto';
+
+export {
+	getInAppMessages,
+	GetInAppMessagesInput,
+	GetInAppMessagesOutput,
+} from './getInAppMessages';
+export { putEvents, PutEventsInput, PutEventsOutput } from './putEvents';
+export {
+	updateEndpoint,
+	UpdateEndpointInput,
+	UpdateEndpointOutput,
+} from './updateEndpoint';
